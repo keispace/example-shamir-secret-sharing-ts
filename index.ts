535faacf9ba25c11e223944e57ba37af7a9c2f39
evan <keispace.kyj@gmail.com>
2025-03-11 15:58:42 +0900

    update package

--- index.ts
+++ index.ts
@@ -1,13 +1,18 @@
-import { BaseCodec } from 'multiformats';
 import { split, combine } from 'shamir-secret-sharing';
+import { base64 } from 'multiformats/bases/base64';
+import { base58btc } from 'multiformats/bases/base58';
+import pako from 'pako';
 
-const loadPackage = async (base: string): Promise<BaseCodec> => {
-    try {
-        const module = await import(`multiformats/bases/${base}`);
-        return module[base];
-    } catch (error) {
-        const module = await import(`multiformats/bases/base64`);
-        return module.base64;
+export type SupportedBase = 'base58' | 'base64';
+const loadPackage = async (base: SupportedBase) => {
+    // base == base256emoji, base64, base32, base16, base58btc
+    switch (base) {
+        case 'base58':
+            return base58btc;
+        case 'base64':
+            return base64;
+        default:
+            throw new Error('Unsupported base');
     }
 };
 
@@ -15,34 +20,55 @@ export const stringToShards64 = async (
     origin: string,
     shares: number,
     threshold: number,
-    base = 'base64',
+    base: SupportedBase = 'base64',
 ): Promise<string[]> => {
     const baseCodec = await loadPackage(base);
-    const secret = new TextEncoder().encode(origin);
-    const shards = await split(secret, shares, threshold);
-    return shards.map((shard) => baseCodec.encoder.baseEncode(shard));
+    const compressed = pako.deflate(origin);
+
+    // const secret = new TextEncoder().encode(compressed);
+    const shards = await split(compressed, shares, threshold);
+    return shards.map((shard) => baseCodec.encode(shard));
 };
 
-export const shards64ToString = async (shardBases: string[], base = 'base64'): Promise<string> => {
+export const shards64ToString = async (shardBases: string[], base: SupportedBase = 'base64'): Promise<string> => {
     const baseModule = await loadPackage(base);
-    const shards = shardBases.map((shard) => baseModule.decoder.baseDecode(shard));
+    const shards = shardBases.map((shard) => baseModule.decode(shard));
     const secret = await combine(shards);
-    return new TextDecoder().decode(secret);
+    return pako.inflate(secret, { to: 'string' });
+
+    // return new TextDecoder().decode(secret);
 };
 
 export const test = async () => {
-    const origin = JSON.stringify({ key: 'value' });
+    const origin = {
+        '@context': ['https://www.w3.org/2018/credentials/v1', 'https://schema.org'],
+        id: 'did:infra:space:5FDseiC76zPek2YYkuyenu4ZgxZ7PUWXt9d19HNB5CaQXt5U',
+        type: ['VerifiableCredential', 'VaccinationCredential'],
+        credentialSubject: {
+            id: 'did:infra:space:5CfVpNJWhHeeb8EWhwKzUk3phDNtUNigCAjnSBYD7S6MnD2d',
+            test: '123',
+            alumniOf: 'Example University',
+            email: 'test@test.com',
+        },
+        issuanceDate: '2023-03-22T06:51:36.019Z',
+    };
+    const originString = JSON.stringify(origin);
     const shares = 5;
     const threshold = 3;
-    const base = 'base58btc';
-    console.log('origin:', origin);
-    const shards = await stringToShards64(origin, shares, threshold, base);
-    console.log('shards:', shards);
+    const base = 'base64' as SupportedBase;
+    const shards = await stringToShards64(originString, shares, threshold, base);
     const partialShards = shards.sort(() => 0.5 - Math.random()).slice(0, threshold);
-    console.log('partial shards:', partialShards);
-
     const restored = await shards64ToString(partialShards, base);
-    console.log('Restored with partial shards:', restored);
+
+    console.log('-------------------');
+    console.log(`use ${base} encoding, Gzip/Deflate and split into ${shares} shards with threshold ${threshold}`);
+    console.log(`origin string(length: ${originString.length}):\n\n${originString}\n`);
+    console.log(`shard length: ${shards[0].length} * 5 = ${shards[0].length * 5}`);
+    console.log(
+        `Match between partial shards(${threshold}) restored data and origin data:  ${restored === originString}`,
+    );
+    console.log('shards:');
+    console.log(shards);
 };
 
 test();
